cb5e2778c020f9f178abdbc4e8410c57ee285c75
Seazie <seazie@SilverLining>
2024-11-08 23:58:37 -0600

    Made some modifications

--- Add.cpp
+++ Add.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "Add.h"
 
 Add::Add() {
@@ -10,4 +11,18 @@ void Add::addExpense(const std::string& name, float amount, const std::string& d
 	std::cout << "Adding Expense - Name: " << name << ", Amount: " << amount << ", Date: " << date << std::endl;
 
 	//logic for saving expense data
+	expense newExpense;
+	newExpense.name = name;
+	newExpense.amount = amount;
+	
+	//Parse date string "MM-DD-YYYY"
+	std::istringstream dateStream(date);
+	char delimiter;
+	dateStream >> newExpense.date.month >> delimiter >> newExpense.date.day >> delimiter >> newExpense.date.year;
+
+	//test to see if it works
+	std::cout << "Expense added - Name: " << newExpense.name
+		<< ", Amount: $" << newExpense.amount
+		<< ", Date: " << newExpense.date.month << "-" << newExpense.date.day << "-" << newExpense.date.year << std::endl;
+	
 }
